3f7cf73200b89f20950a17f062d694986f4d7772
sarango25 <arangosebastian2001@gmail.com>
2021-12-04 17:44:37 -0500

    codigo no finalizado

---
+++ scr/index.js
@@ -0,0 +1,18 @@
+
+//DECLARACIONES
+const estudiante = document.getElementById('estudiante');
+const codigo = document.getElementById('codigo');
+const curso = document.getElementById('curso');
+const matricularBtn = document.getElementById('matricularBtn');
+
+matricular = () => {
+    alert('Exito!');
+
+}
+
+matricularBtn.addEventListener('click', matricular);
+
+//Me disculpo de antemano por entregar un código tan incompleto. Tuve un error intentando conectar JS con la base de datos, al momento de ingresar "npm run start" en la terminal no me funcionó. De ahi en adelante el código no me volvió a responder.
+
+
+
